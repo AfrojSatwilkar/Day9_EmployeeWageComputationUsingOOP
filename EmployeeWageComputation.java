1bdd1d0ee97a9893c8e09a54f8470eaa557e699d
Afroj Satwilkar <88261856+AfrojSatwilkar@users.noreply.github.com>
2021-10-09 11:01:47 +0530

    Update EmployeeWageComputation.java
    Day9 Emp wage for company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,6 +1,6 @@
 package com.company1.EmployeeWageComputationUsingOOps;
 
-public class EmployeeWageComputation {
+public class EmployeeWageComputation  {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program using OOP");
